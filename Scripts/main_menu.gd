cfdbdb889dd53e514ec625b6133f7442360de02e
Ryan Evans <112006148+RynEEvans@users.noreply.github.com>
2025-05-04 03:51:51 -0400

    Finished BAD Part 1 Template
    Finished making Battery Acid Dev's Multiplayer Template Part 1.

---
+++ Scripts/main_menu.gd
@@ -0,0 +1,23 @@
+extends Control
+
+const GAME_SCENE = "res://Scenes/game.tscn"
+
+func ready():
+	if OS.has_feature("dedicated_server"):
+		print("Calling Host game...")
+		NetworkManager.host_game()
+		get_tree().call_deferred(&"change_scene_to_packed", preload(GAME_SCENE))
+
+func host_game():
+	print("Host game pressed")
+	NetworkManager.host_game()
+	get_tree().call_deferred(&"change_scene_to_packed", preload(GAME_SCENE))
+	
+func join_game():
+	print("Join game pressed")
+	NetworkManager.join_game()
+	get_tree().call_deferred(&"change_scene_to_packed", preload(GAME_SCENE))
+
+
+func exit_game():
+	get_tree().quit(0)
